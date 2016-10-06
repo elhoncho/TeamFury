29229262db05eb5881e70d3a9ea76cf4f22bacdf
sabyers <saul@jrbrothers.com>
2016-10-06 00:23:55 -0500

    Simple forward only motion complete, without timing

--- DirectionControl.v
+++ DirectionControl.v
@@ -6,36 +6,39 @@
 module DirectionControl(
 		input clk,
 		input RFS, 
-		//input RRS, 
+		input RRS, 
 		input RMS, 
 		input LMS,
 		input LFS, 
-		//input LRS,
+		input LRS,
 		//input Forward,
 		//input Backward,
 		output reg [3:0]DIR
    );
 	
-	reg [24:0] CountOne = 0;
+	reg [24:0] CountOne = 0; //Time Delay for input signal
 	reg [24:0] CountTwo = 0;
+   reg [24:0] CountFinal = 0;
 	reg [27:0] Count90 = 0;
-	reg [3:0] StableOut; //Stable output
-	reg [3:0] StableOut2; //Stable output
-	reg [3:0] Signal; //Used to store realtime signal
-	reg [1:0] Test = 0;
+   reg [5:0] UnstableIn; //Unstable input
+	reg [5:0] StableOut; //Stable output
+	reg [5:0] Signal; //Used to store stable input signal
+	reg [1:0] Test;
 
 	parameter MAX_COUNT = 12_500_000; // 500 ms time delay
-	parameter CORNER_TIMER = 50_000_000;
+	parameter CORNER_TIMER = 50_000_000; //Detect 90 or intersect
 	
 	always@(posedge clk)begin
-		Signal[0] <= LMS;	//[2]
-		Signal[1] <= RMS;	//[3]
-		Signal[2] <= LFS; //[4]
-		Signal[3] <= RFS;	//[5]
-		//Signal[0] <= LMS;
-		//Signal[1] <= RMS;
-		StableOut2 <= ~Signal;	
-
+	   //Signal inputs
+		UnstableIn[0] <= LRS;
+		UnstableIn[1] <= RRS;
+		UnstableIn[2] <= LMS; 
+		UnstableIn[3] <= RMS;
+		UnstableIn[4] <= LFS;
+		UnstableIn[5] <= RFS;
+		//Stabilized input
+		Signal <= UnstableIn;
+		
 		if (StableOut != Signal)begin //test for change in signal
 			if(CountOne < MAX_COUNT) begin //Delay count 1
 				CountOne <= CountOne + 1;
@@ -47,14 +50,22 @@ module DirectionControl(
 					Test <= 2;
 				end
 				else if (StableOut != Signal && Test >= 1)begin //Signal Still there?
-					StableOut <= ~Signal; //Place signal input to LED output
-					CountOne <= 0;	//Reset count values
-					CountTwo <= 0;
+						  if (CountFinal < MAX_COUNT) begin
+								CountFinal <= CountFinal + 1;
+								Test <= 3;
+						  end
+						  else if (StableOut != Signal && Test >= 2) begin //Signal Still there?
+									StableOut <= ~Signal; //Place signal input to LED output
+									CountOne <= 0;	//Reset count values
+									CountTwo <= 0;
+									CountFinal <= 0;
+									Test <= Test + 1; //Keeps compiler happy
+					end
 				end
 			end
 		end
-		else if (Signal == 4'b11_11) begin //No signal
-			StableOut <= 4'b00_00;			//Turn LEDS off
+		else if (Signal == 6'b11_11_11) begin //No signal
+			StableOut <= 6'b00_00_00;			//Turn LEDS off
 		end		
 		
 		//00 Proceed 00 Full
@@ -63,41 +74,41 @@ module DirectionControl(
 		//11 Stop 11 90 degree
 		
 		casex (StableOut)
-			
 			//Proceed
-			4'b11_??: DIR = 4'b00_00;
-		
+			6'b11_??_??: DIR = 4'b00_00;
 			//Veer Left
-			4'b10_??: DIR = 4'b01_01;
-			
+			6'b10_??_??: DIR = 4'b01_01;
 			//Veer Right
-			4'b01_??: DIR = 4'b10_01;
-			
+			6'b01_??_??: DIR = 4'b10_01;
 			//90 degree or intersect		
-			4'b00_??: begin
-							DIR = 4'b00_00;
+			6'b00_??_??: begin
 								if (Count90 <= CORNER_TIMER) begin
-									if (StableOut == 4'b00_01) begin
-										DIR = 4'b01_11;
-									end
-									else if (StableOut == 4'b00_10) begin
-										DIR = 4'b10_11;
-									end
-									Count90 <= Count90 + 1;
+									 casex (StableOut)
+											  //90 degree left
+											  6'b00_01_??: DIR = 4'b01_11;
+											  //90 degree right
+											  6'b00_10_??: DIR = 4'b10_11;
+												//Hold time
+												default: begin
+													DIR = 4'b00_00;
+													Count90 <= Count90 + 1;
+												end
+									endcase
 								end
-								else if (StableOut == 4'b00_00) begin
-									  DIR = 4'b11_11;
-								end
-								else if (StableOut == 4'b00_01) begin
-										DIR = 4'b01_11;
-								end
-								else if (StableOut == 4'b00_10) begin
-										DIR = 4'b10_11;
-								end
-									if(StableOut != StableOut2) begin
+								else 
+										casex (StableOut)
+												//90 degree left
+												6'b00_01_??: DIR = 4'b01_11;
+												//90 degree right
+												6'b00_10_??: DIR = 4'b10_11;
+												//Stop
+												default: DIR = 4'b11_11;
+										endcase
+												
+									if(StableOut != ~Signal) begin //Reset count for next signal
 									   Count90 <= 0;
-								   end
-						 end
+									end	
+						      end
 			
 			default: DIR = 4'b11_11;
 		endcase
